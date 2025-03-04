407e7f0c803d3afd6a55bd3552cd590fddb42184
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-04 15:33:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 from vllm import LLM
 from vllm.sampling_params import SamplingParams
-    
+from transformers import AutoTokenizer
+
 class InferlessPythonModel:
     def initialize(self):
         model_id = "microsoft/phi-4"
         self.llm = LLM(model=model_id,enforce_eager=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     def infer(self, inputs):
-        prompts = inputs["prompt"]
+        prompt = inputs["prompt"]
+        system_prompt = inputs["system_prompt"]
         temperature = inputs.get("temperature",0.7)
         top_p = inputs.get("top_p",0.1)
         repetition_penalty = inputs.get("repetition_penalty",1.18)
         top_k = int(inputs.get("top_k",40))
         max_tokens = inputs.get("max_tokens",256)
 
+        chat_message = [
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt}
+        ]
+
         sampling_params = SamplingParams(temperature=temperature,top_p=top_p,
                                          repetition_penalty=repetition_penalty,
                                          top_k=top_k,max_tokens=max_tokens
                                         )
-        result = self.llm.generate(prompts, sampling_params)
+        chat_format = self.tokenizer.apply_chat_template(chat_message,tokenize=False,add_generation_prompt=True)
+        result = self.llm.generate(chat_format, sampling_params)
         result_output = [output.outputs[0].text for output in result]
         
         return {"generated_text":result_output[0]}

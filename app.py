152f709d988a6a949c67159ca9b87ddcf8ac1d1f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2025-03-05 12:04:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from vllm import LLM
 from vllm.sampling_params import SamplingParams
 from transformers import AutoTokenizer
+from time import perf
 
 class InferlessPythonModel:
     def initialize(self):
@@ -9,6 +10,7 @@ class InferlessPythonModel:
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     def infer(self, inputs):
+        token_time_st = perf_counter()
         prompt = inputs["prompt"]
         system_prompt = inputs.get("system_prompt","You are a friendly bot.")
         temperature = inputs.get("temperature",0.7)
@@ -27,10 +29,17 @@ class InferlessPythonModel:
                                          top_k=top_k,max_tokens=max_tokens
                                         )
         chat_format = self.tokenizer.apply_chat_template(chat_message,tokenize=False,add_generation_prompt=True)
+        token_time_rt = perf_counter() - token_time_st
+        
+        result_time_st = perf_counter()
         result = self.llm.generate(chat_format, sampling_params)
         result_output = [output.outputs[0].text for output in result]
+        result_time_rt = perf_counter() - result_time_st
         
-        return {"generated_text":result_output[0]}
+        return {"generated_text":result_output[0],
+               "token_time":token_time_rt,
+               "result_time":result_time_rt
+               }
         
     def finalize(self):
         self.llm = None
